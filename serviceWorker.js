03664f05796be24091b1add3a7606d57222fbcae
Esmail keyhan <Esmailkeyhan22@gmail.com>
2025-04-29 07:42:38 +0430

    create app

---
+++ serviceWorker.js
@@ -0,0 +1,31 @@
+const staticCacheName = "site-static-v1";
+const cacheAssets = [
+    "index.html",
+    "assets/stylesheet/app.css",
+    "assets/img/lg.png.jpg"
+];
+self.addEventListener("install", evt => {
+    evt.waitUntil(
+        caches
+            .open(staticCacheName)
+            .then(cache => {
+                console.log("caching assets...");
+                cache.addAll(cacheAssets);
+            })
+            .catch(err => { })
+    );
+});
+self.addEventListener("fetch", evt => {
+    evt.respondWith(
+        caches
+            .match(evt.request)
+            .then(res => {
+                return res || fetch(evt.request);
+            })
+            .catch(err => {
+                if (evt.request.url.indexOf(".html") > -1) {
+                    return caches.match("./index.html");
+                }
+            })
+    );
+});
